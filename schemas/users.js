5d0223f685aa692436b5b49d1bc01eb7f314145e
semicirculomx <enriquerioszam@gmail.com>
2024-12-17 20:27:10 -0600

    age verification

--- schemas/users.js
+++ schemas/users.js
@@ -1,9 +1,9 @@
 import joi from 'joi';
 
 export const userSignUp = joi.object({
-    name: joi.string().min(4).max(30).required(),
+    name: joi.string().min(3).max(30).required(),
     email: joi.string().email({ minDomainSegments: 2 }).required(),
-    password: joi.string().min(8).max(25).required(),
+    password: joi.string().min(6).max(30).required(),
     phone: joi.string().pattern(/^[0-9]{10,15}$/).required(), // Ejemplo: entre 10 y 15 dígitos
     ageVerified: joi.boolean().required(),
 });
