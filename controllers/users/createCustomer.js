d9384028b6e3ae757506011c4151cb0b1b827501
semicirculomx <enriquerioszam@gmail.com>
2025-01-13 15:28:56 -0600

    verifycode adding on dashboard create customer

--- controllers/users/createCustomer.js
+++ controllers/users/createCustomer.js
@@ -1,7 +1,21 @@
 import User from '../../models/User.js'
 import bcryptjs from 'bcryptjs';
+import crypto from 'crypto';
+
+function generateFourDigitCode() {
+    let code;
+    do {
+        // Genera 2 bytes de datos aleatorios
+        const randomBytes = crypto.randomBytes(2);
+        // Convierte los bytes en un número entero sin signo de 16 bits
+        code = randomBytes.readUInt16BE(0);
+    } while (code < 1000 || code > 9999);
+    return code;
+}
 
 let createCustomer = async (req, res, next) => {
+    req.body.verify_code = generateFourDigitCode()
+
     console.log(req.body)
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
     try {
