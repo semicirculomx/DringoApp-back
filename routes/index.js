2e720880b919114dbab3ac28e56403bdb2c94f5d
jorzarios <jorzarios@gmail.com>
2025-02-14 18:31:49 -0600

    check version

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ import favoritesRouter from './favorites.js'
 import paymentsRouter from './payments.js'
 import ordersRoutes from './orders.js'
 import bannersRoutes from './banners.js'
+import utilsRoutes from './utils.js'
 // Creamos un nuevo objeto router
 const router = express.Router();
 
@@ -22,6 +23,6 @@ router.use('/coupons', couponsRouter)
 router.use('/payments', paymentsRouter)
 router.use('/orders', ordersRoutes)
 router.use('/banners', bannersRoutes)
-
+router.use('/utils', utilsRoutes)
 // Exportamos el objeto router
 export default router;
