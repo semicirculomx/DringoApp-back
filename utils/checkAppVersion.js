d344de7a6c277f7c17102e01b911e3d3a1a42437
jorzarios <jorzarios@gmail.com>
2025-02-14 21:10:47 -0600

    adding country

--- utils/checkAppVersion.js
+++ utils/checkAppVersion.js
@@ -1,4 +1,4 @@
-const APP_STORE_LOOKUP_URL = "https://itunes.apple.com/lookup?id=6739433324";
+const APP_STORE_LOOKUP_URL = "https://itunes.apple.com/lookup?id=6739433324&country=mx";
 let cachedVersion = null;
 let lastChecked = 0;
 const CACHE_DURATION = 60 * 60 * 1000; // 1 hora
