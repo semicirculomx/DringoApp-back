350d1d8558cf145877c0a897fe5c65db0f044f6b
jorzarios <jorzarios@gmail.com>
2025-02-20 14:07:29 -0600

    delete required on category and sub category

--- models/Category.js
+++ models/Category.js
@@ -3,8 +3,8 @@ const { Schema, model } = mongoose;
 
 const categorySchema = new Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
-  description:{type:String, required: true},
+  image: { type: String},
+  description:{type:String},
 }, { timestamps: true });
 
 const Category = model('Category', categorySchema);
--- models/SubCategory.js
+++ models/SubCategory.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const subcategorySchema = new Schema({
-    name: { type: String, required: true },
-  image: { type: String, required: true },
-  description:{type:String, required: true},
+    name: { type: String },
+  image: { type: String },
+  description:{type:String},
 }, { timestamps: true });
 
 const Subcategory = model('Subcategory', subcategorySchema);
